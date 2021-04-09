09dd04668094f3bf40cc9f21c5b9dd1444c2a83d
kia4545 <81552960+kia4545@users.noreply.github.com>
2021-04-09 19:03:30 +0900

    PHP基礎Q9～Q16まで終了

--- task.php
+++ task.php
@@ -93,28 +93,66 @@ echo PHP_EOL;
 print("#####q9#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
-  # 以下に回答を記載
+$names1 = [];
+foreach ($names as $key => $name) {
+  $index = $key + 1;
+  $names1[] =  "会員No. " . $index . $name;
+  array_push($names, $index);
+}
+print_r($names1);
 
 echo PHP_EOL;
 
 print("#####q10#####".PHP_EOL);
 $foods = ["いか","たこ","うに","しゃけ","うにぎり","うに軍艦","うに丼","高級なうに"];
 
-  # 以下に回答を記載
+foreach ($foods as $food) {
+  if (preg_match('/うに/', $food)) {
+    echo "好物です", PHP_EOL;
+  } else {
+    echo "まぁまぁ好きです", PHP_EOL;
+  }
+}
 
 echo PHP_EOL;
 
 print("#####q11#####".PHP_EOL);
 $sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
 
-  # 以下に回答を記載
+$sports1 = [];
+foreach ($sports as $key => $sport) {
+  if (is_array($sport)) {
+    $sports1 = array_merge($sports1, $sport);
+  } else {
+    array_push($sports1, $sport);
+  }
+}
+//配列で重複を削除
+$sports1 = array_unique($sports1);
+//バラバラの要素にキーを振り直す
+$sports1 = array_values($sports1);
+
+$sports2 = [];
+$index = [];
+foreach ($sports1 as $key => $sport) {
+  $index = $key + 1;
+  $sports2[] =  "No. " . $index . $sport;
+  array_push($sports1, $index);
+}
+print_r("ユーザの趣味一覧 ");
+foreach ($sports2 as $sport) {
+  print($sport . PHP_EOL);
+}
 
 echo PHP_EOL;
 
 print("#####q12#####".PHP_EOL);
 $data = [ "user" => [ "name" => "satou", "age" => 33 ] ];
 
-  # 以下に回答を記載
+//型の初期化
+$name = [];
+$name = $data["user"]["name"];
+print_r($name . PHP_EOL);
 
 echo PHP_EOL;
 
@@ -122,14 +160,28 @@ print("#####q13#####".PHP_EOL);
 $user_data = [ "name" => "神里", "age" => 31, "address" => "埼玉"];
 $update_data = [ "age" => 32, "address" => "沖縄" ];
 
-  # 以下に回答を記載
+//要素を置き換える
+$update_data = array_replace($user_data, $update_data);
+print_r($update_data);
+
 
 echo PHP_EOL;
 
 print("#####q14#####".PHP_EOL);
 $data = [ "name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com" ];
 
-  # 以下に回答を記載
+//型の初期化
+$data_key = [];
+$data_key1 = [];
+
+//キーを取得
+$data_key = array_keys($data);
+//キーを振り直す
+$data_key1 = array_values($data);
+//配列に追加
+array_push($data, $data_key1);
+print_r($data_key1);
+
 
 echo PHP_EOL;
 
@@ -137,7 +189,33 @@ print("#####q15#####".PHP_EOL);
 $data1 = [ "name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin" ];
 $data2 = [ "name" => "yamada", "hobby" => "baseball", "role" => "normal" ];
 
-  # 以下に回答を記載
+//型の初期化
+$data1_1 = [];
+//キーを取得
+$data1_1 = array_keys($data1);
+//配列に追加
+array_push($data1);
+
+//条件分岐＆正規表現
+if (preg_grep('/age/', $data1_1)) {
+  echo "OK";
+} else {
+  echo "NG";
+}
+
+//型の初期化
+$data2_1 = [];
+//キーを取得
+$data2_1 = array_keys($data2);
+//配列に追加
+array_push($data2);
+
+//条件分岐＆正規表現
+if (preg_grep('/age/', $data2_1)) {
+  echo "OK";
+} else {
+  echo "NG";
+}
 
 echo PHP_EOL;
 
@@ -149,7 +227,10 @@ $users = [
   [ "name" => "nakamura", "age" => 41 ]
 ];
 
-  # 以下に回答を記載
+foreach ($users as $user) {
+  echo "私の名前は" . $user["name"] . "です。年齢は" . $user["age"] . "歳です。" . PHP_EOL;
+}
+
 
 echo PHP_EOL;
 
